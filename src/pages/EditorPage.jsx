4f82c31e7077e2310a8930c8f6d162dfcc8c58cc
manishkumarth <manish12099@gmail.com>
2025-09-18 10:42:22 +0530

    drag-drop-feature update

--- src/pages/EditorPage.jsx
+++ src/pages/EditorPage.jsx
@@ -4,19 +4,19 @@ import PrevieCard from "../EditorPageComponents/previeCard";
 
 function EditorPage() {
   return (
-    <div style={{widh:"100vw", height:"80vh",border:"1px solid red", position:"absolute",top:"9%"}}>
+    <div className="container mx-auto mt-5" style={{ widh: "100vw", height: "100vh" }}>
       <EditorSideBar id="sidebarcomponent" className="mt-4" />
-      <div className="container">
-        <div className="row">
-          <div className="col-6">
-        
+      <div className="container-fluid mt-5 px-1">
+        <div className="row ms-5" style={{ border: "1px solid red", marginTop: "69px" }}>
+          <div className="col-lg-6 col-md-12 col-sm-12 border" style={{ height: "80vh" }}>
             <EditTemplate />
           </div>
-          <div className="col-6">
+          <div className="col-lg-6 col-md-12 col-sm-12 border" style={{ height: "80vh" }}>
             <PrevieCard />
           </div>
         </div>
       </div>
+
     </div>
   );
 }
